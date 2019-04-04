80f137e7cf16e1d5cc69b6d307366ce30fa5907a
Rob Currie <rcurrie@ucsc.edu>
2019-04-04 07:39:33 -0700

    Move contract json into build

--- truffle-config.js
+++ truffle-config.js
@@ -3,21 +3,12 @@ var HDWalletProvider = require("truffle-hdwallet-provider");
 const path = require("path");
 
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
-  contracts_build_directory: path.join(__dirname, "src/contracts"),
+  contracts_build_directory: path.join(__dirname, "build/contracts"),
   networks: {
     local: {
       host: "127.0.0.1",
       port: 18545,
       network_id: "*"
-    },
-    ropsten: {
-      provider: () => new HDWalletProvider(
-        process.env.MNENOMIC, "https://ropsten.infura.io/v3/" + process.env.INFURA_API_KEY),
-      network_id: 3,
-      gas: 4700000,
-      gasPrice: 100000000000
     },
     rinkeby: {
       provider: () => new HDWalletProvider(
@@ -25,6 +16,13 @@ module.exports = {
       network_id: 4,
       gas: 4500000,
       gasPrice: 10000000000,
+    },
+    ropsten: {
+      provider: () => new HDWalletProvider(
+        process.env.MNENOMIC, "https://ropsten.infura.io/v3/" + process.env.INFURA_API_KEY),
+      network_id: 3,
+      gas: 4700000,
+      gasPrice: 100000000000
     }
   }
 };
