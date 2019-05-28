84c7efa50a440482a4530033874d044c98f4b4c4
Rob Currie <rcurrie@ucsc.edu>
2019-05-28 16:05:13 -0700

    Updates

---
+++ precache-manifest.3081ea25566a80f9b0a1118851d986ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "897c12a3bb4562fc3fd3c83b55fb2080",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3c39ab61367bcdce8c7",
+    "url": "/static/css/2.1e66eaca.chunk.css"
+  },
+  {
+    "revision": "d3c39ab61367bcdce8c7",
+    "url": "/static/js/2.0bc88332.chunk.js"
+  },
+  {
+    "revision": "9e1e6ea37c0fd6b77edb",
+    "url": "/static/js/main.967143b1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
