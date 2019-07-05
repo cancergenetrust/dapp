6dde4ea4cf7d7a25fe6993c2972a2546ebe248c0
Rob Currie <rcurrie@ucsc.edu>
2019-07-04 19:12:51 -0700

    Downgrade to web3 1.0.0-beta.34 to resolve CORS issues when accessing infura from Safari and Firefox

--- src/getWeb3.js
+++ src/getWeb3.js
@@ -4,27 +4,20 @@ const getWeb3 = () =>
   new Promise((resolve, reject) => {
     // Wait for loading completion to avoid race conditions with web3 injection timing.
     window.addEventListener("load", async () => {
-      // Modern dapp browsers...
       if (window.ethereum) {
         console.log("window.ethereum detected...");
-        // const web3 = new Web3(window.ethereum);
-        const web3 = new Web3(window.web3.currentProvider);
+        const web3 = new Web3(window.ethereum);
         try {
-          // Request account access if needed
-          await window.ethereum.enable();
+          // Request account access if needed - but we are read only so no need
+          // await window.ethereum.enable();
           // Acccounts now exposed
           resolve(web3);
+          console.log("web3 resolved using version", web3.version);
         } catch (error) {
           reject(error);
         }
       }
-      // Legacy dapp browsers...
-      else if (window.web3) {
-        console.log("window.web3 legacy detected...");
-        const web3 = window.web3;
-        resolve(web3);
-      }
-      // Fallback to localhost; use dev console port by default...
+      // If no metamastk then default to rinkeby now, main net later
       else {
         console.log("No injected web3 detected, using integrated web3 connected to Rinkeby test network...");
         const provider = new Web3.providers.HttpProvider("https://rinkeby.infura.io");
--- truffle-config.js
+++ truffle-config.js
@@ -5,7 +5,7 @@ const path = require("path");
 module.exports = {
   contracts_build_directory: path.join(__dirname, "src/contracts"),
   networks: {
-    develop: {
+    local: {
       host: "127.0.0.1",
       port: 18545,
       network_id: "*"
